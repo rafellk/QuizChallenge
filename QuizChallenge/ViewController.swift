5b2ee8fd70ea4ae4b78a7a8f7c28cb8ba1e2a9b3
Rafael Lucena <rafellk@gmail.com>
2019-12-09 20:59:17 -0300

    Initial project structure

---
+++ QuizChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizChallenge
+//
+//  Created by Rafael Lucena on 12/9/19.
+//  Copyright © 2019 RLMG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
